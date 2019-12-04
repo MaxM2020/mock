3bed8c4fe54632d27737d5991051d40ec12a322d
Guna <gunanidhi10@gmail.com>
2019-12-04 12:02:02 +0530

    Intial Commit

---
+++ TestGit/src/Student.java
@@ -0,0 +1,14 @@
+
+class TestStudent {	
+	 int id;  
+	 String name;
+
+}
+public class Student{  
+	 public static void main(String args[]){  
+		 TestStudent s1=new TestStudent();  
+		 s1.id=101;  
+		 s1.name="Sonoo";  
+		 System.out.println(s1.id+" "+s1.name);//printing members with a white space  
+	 }  
+	}  
